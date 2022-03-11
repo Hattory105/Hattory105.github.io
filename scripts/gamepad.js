0dade25a33a99a245042f8c29b94f7f167ada1fe
Hattory105 <59718562+Hattory105@users.noreply.github.com>
2022-03-11 09:54:00 +0700

    Update

--- scripts/gamepad.js
+++ scripts/gamepad.js
@@ -23,25 +23,25 @@ var enableGamepad = false;
 
 var gamepadBtn = [
     //object infor, x, y, w, h
-    {imgID: "imgBGDPad", draw: true, touchEnable: false, name: "dpad_pos_bg", x: 155, y: 60, w: 462, h: 462},
-    {imgID: "imgDPadNormal", draw: true, touchEnable: false, name: "dpad_pos", x: 200, y: 100, w: 380, h: 380},
-    {imgID: "", draw: false, touchEnable: true, name: "dpad_left", keyCode: KEYCODE_DPAD_LEFT, x: 200, y: 230, w: 130, h: 120},
-    {imgID: "", draw: false, touchEnable: true, name: "dpad_right", keyCode: KEYCODE_DPAD_RIGHT, x: 450, y: 230, w: 130, h: 120},
-    {imgID: "", draw: false, touchEnable: true, name: "dpad_up", keyCode: KEYCODE_DPAD_UP, x: 330, y: 100, w: 120, h: 130},
-    {imgID: "", draw: false, touchEnable: true, name: "dpad_down", keyCode: KEYCODE_DPAD_DOWN, x: 330, y: 350, w: 120, h: 130},
-    {imgID: "imgL", draw: true, touchEnable: true, name: "trigger_l", keyCode: KEYCODE_BUTTON_L1, x: 28, y: 458, w: 115, h: 224},
-    {imgID: "imgR", draw: true, touchEnable: true, name: "trigger_r", keyCode: KEYCODE_BUTTON_R1, x: 1773, y: 458, w: 115, h: 224},
+    {imgIDHL: "", imgID: "imgBGDPad", draw: true, touchEnable: false, name: "dpad_pos_bg", x: 155, y: 60, w: 462, h: 462},
+    {imgIDHL: "", imgID: "imgDPadNormal", draw: true, touchEnable: false, name: "dpad_pos", x: 200, y: 100, w: 380, h: 380},
+    {imgIDHL: "imgDPadLeft", imgID: "", draw: false, touchEnable: true, name: "dpad_left", keyCode: KEYCODE_DPAD_LEFT, x: 200, y: 230, w: 130, h: 120},
+    {imgIDHL: "imgDPadRight", imgID: "", draw: false, touchEnable: true, name: "dpad_right", keyCode: KEYCODE_DPAD_RIGHT, x: 450, y: 230, w: 130, h: 120},
+    {imgIDHL: "imgDPadUp", imgID: "", draw: false, touchEnable: true, name: "dpad_up", keyCode: KEYCODE_DPAD_UP, x: 330, y: 100, w: 120, h: 130},
+    {imgIDHL: "imgDPadDown", imgID: "", draw: false, touchEnable: true, name: "dpad_down", keyCode: KEYCODE_DPAD_DOWN, x: 330, y: 350, w: 120, h: 130},
+    {imgIDHL: "imgLHL", imgID: "imgL", draw: true, touchEnable: true, name: "trigger_l", keyCode: KEYCODE_BUTTON_L1, x: 28, y: 458, w: 115, h: 224},
+    {imgIDHL: "imgRHL", imgID: "imgR", draw: true, touchEnable: true, name: "trigger_r", keyCode: KEYCODE_BUTTON_R1, x: 1773, y: 458, w: 115, h: 224},
 
     //Keycode for press/release, touch enable?, name, x, y, w, h, xR, yR
-    {imgID: "imgX", draw: true, touchEnable: true, name: "button_x", keyCode: KEYCODE_BUTTON_X, x: 1168, y: 204, w: 224, h: 224, xR: 1280, yR: 316},
-    {imgID: "imgY", draw: true, touchEnable: true, name: "button_y", keyCode: KEYCODE_BUTTON_Y, x: 1355, y: 48, w: 224, h: 224, xR: 1467, yR: 160},
-    {imgID: "imgA", draw: true, touchEnable: true, name: "button_a", keyCode: KEYCODE_BUTTON_A, x: 1355, y: 360, w: 224, h: 224, xR: 1467, yR: 472},
-    {imgID: "imgB", draw: true, touchEnable: true, name: "button_b", keyCode: KEYCODE_BUTTON_B, x: 1536, y: 204, w: 224, h: 224, xR: 1648, yR: 316},
-    {imgID: "imgBack", draw: true, touchEnable: true, name: "button_back", keyCode: KEYCODE_BUTTON_SELECT, x: 870, y: 480, w: 180, h: 180, xR: 960, yR: 570},
-    {imgID: "imgBGMove", draw: true, touchEnable: false, name: "button_axis_left_bg", x: 350, y: 570, w: 464, h: 464, xR: 582, yR: 802},
-    {imgID: "imgBGMove", draw: true, touchEnable: false, name: "button_axis_right_bg", x: 1096, y: 570, w: 464, h: 464, xR: 1328, yR: 802},
-    {imgID: "imgControll", draw: true, touchEnable: false, name: "button_axis_left", keyCode: 0, x: 449, y: 669, w: 266, h: 266, xR: 582, yR: 802},
-    {imgID: "imgControll", draw: true, touchEnable: false, name: "button_axis_right", keyCode: 0, x: 1195, y: 669, w: 266, h: 266, xR: 1328, yR: 802},
+    {imgIDHL: "imgXHL", imgID: "imgX", draw: true, touchEnable: true, name: "button_x", keyCode: KEYCODE_BUTTON_X, x: 1168, y: 204, w: 224, h: 224, xR: 1280, yR: 316},
+    {imgIDHL: "imgYHL", imgID: "imgY", draw: true, touchEnable: true, name: "button_y", keyCode: KEYCODE_BUTTON_Y, x: 1355, y: 48, w: 224, h: 224, xR: 1467, yR: 160},
+    {imgIDHL: "imgAHL", imgID: "imgA", draw: true, touchEnable: true, name: "button_a", keyCode: KEYCODE_BUTTON_A, x: 1355, y: 360, w: 224, h: 224, xR: 1467, yR: 472},
+    {imgIDHL: "imgBHL", imgID: "imgB", draw: true, touchEnable: true, name: "button_b", keyCode: KEYCODE_BUTTON_B, x: 1536, y: 204, w: 224, h: 224, xR: 1648, yR: 316},
+    {imgIDHL: "imgBackHL", imgID: "imgBack", draw: true, touchEnable: true, name: "button_back", keyCode: KEYCODE_BUTTON_SELECT, x: 870, y: 480, w: 180, h: 180, xR: 960, yR: 570},
+    {imgIDHL: "", imgID: "imgBGMove", draw: true, touchEnable: false, name: "button_axis_left_bg", x: 350, y: 570, w: 464, h: 464, xR: 582, yR: 802},
+    {imgIDHL: "", imgID: "imgBGMove", draw: true, touchEnable: false, name: "button_axis_right_bg", x: 1096, y: 570, w: 464, h: 464, xR: 1328, yR: 802},
+    {imgIDHL: "", imgID: "imgControll", draw: true, touchEnable: false, name: "button_axis_left", keyCode: 0, x: 449, y: 669, w: 266, h: 266, xR: 582, yR: 802},
+    {imgIDHL: "", imgID: "imgControll", draw: true, touchEnable: false, name: "button_axis_right", keyCode: 0, x: 1195, y: 669, w: 266, h: 266, xR: 1328, yR: 802},
 ];
 
 var gamepadBtnScaled = [];
@@ -70,6 +70,7 @@ function updateGamepadScale() {
     for(let i = 0; i < gamepadBtn.length; i++) {
         gamepadBtnScaled[i] = {};
         gamepadBtnScaled[i].imgID = gamepadBtn[i].imgID;
+        gamepadBtnScaled[i].imgIDHL = gamepadBtn[i].imgIDHL;
         gamepadBtnScaled[i].draw = gamepadBtn[i].draw;
         gamepadBtnScaled[i].touchEnable = gamepadBtn[i].touchEnable;
         gamepadBtnScaled[i].name = gamepadBtn[i].name;
@@ -133,15 +134,41 @@ function checkGamepad(typetouch, x, y, x2, y2) {
                 if(item["xR"] != null && item["yR"] != null) { //button Arc as: X, Y, A, B, Back, Axis Left/Right
                     if(checkPointInArc(xTouch, yTouch, item.xR, item.yR, item.w < item.h ? item.w / 2 : item.h / 2) == true) {
                         log(typetouch, item.keyCode, item.name);
+                        drawGamepadItem(typetouch, item);
+                        
                     }
                 } else { //button rect as: left/right/up/down, Trigger L/R
                     if(checkPointInRect(xTouch, yTouch, item.x, item.y, item.w, item.h) == true) {
                         log(typetouch, item.keyCode, item.name);
+                        drawGamepadItem(typetouch, item);
                     }
                 }
             }
         }
     }
+}
+
+function drawGamepadItem(typetouch, item) {
+    let imgIDDraw = item.imgID;
+    let itemDraw = item;
+    if(typetouch == "touchstart") {
+        imgIDDraw = item.imgIDHL;
+    }
+    if(item.name == "button_axis_left" || item.name == "button_axis_right") { //Don't has highlight to draw
+        return;
+    }
+
+    if(item.name == "dpad_left" || item.name == "dpad_right" || item.name == "dpad_up" || item.name == "dpad_down") {
+        //Use x, y, w, h of imgDPadNormal
+        itemDraw = gamepadBtnScaled[1];
+        if(typetouch == "touchend") {
+            imgIDDraw = gamepadBtnScaled[1].imgID;
+        }
+        //Draw dpad bg:
+        let itemDpadBG = gamepadBtnScaled[0];
+        ctx.drawImage(document.getElementById(itemDpadBG.imgID), itemDpadBG.x, itemDpadBG.y, itemDpadBG.w, itemDpadBG.h);    
+    }
+    ctx.drawImage(document.getElementById(imgIDDraw), itemDraw.x, itemDraw.y, itemDraw.w, itemDraw.h);
 }
 
 function onTestGamepad() {
